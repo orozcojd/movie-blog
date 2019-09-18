3387724ac79c4e9053bdcd1648f43b1c3f230e1f
Jonathan Orozco <socaljorozco@gmail.com>
2019-09-18 13:21:18 -0700

    updated root_api route

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
 	NODE_ENV: '"production"',
-	ROOT_API: '"https://edge-route-threepointo.b9ad.pro-us-east-1.openshiftapps.com/"'
+	ROOT_API: '"http://threepointo-qa-threepointo-qa.b9ad.pro-us-east-1.openshiftapps.com/"'
 }
