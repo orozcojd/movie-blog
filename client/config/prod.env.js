a90c0fe24a065a9ddd1d06ce36d8ffde205f278f
Jonathan Orozco <socaljorozco@gmail.com>
2019-09-18 20:16:06 -0700

    changed prod site ref in env variable.

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
 	NODE_ENV: '"production"',
-	ROOT_API: '"http://threepointo-qa-threepointo-qa.b9ad.pro-us-east-1.openshiftapps.com/"'
+	ROOT_API: process.env.ROOT_API
 }
